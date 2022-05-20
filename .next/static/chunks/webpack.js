816d5f82aab79761a3497c0ffb0e80e23b21e7d3
Ashirmatov18 <beksultanashirmatov@gmail.com>
2022-05-20 10:05:15 +0600

    modal

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4f4ff7f4fe0153c0"; }
+/******/ 		__webpack_require__.h = function() { return "a8e08a03884e375e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
